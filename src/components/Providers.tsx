f210301152e62c7353038e5ae62926379ad1a2f6
Maxim <toper.one111@gmail.com>
2023-12-13 11:17:36 +0200

    Render out a post feed

--- src/components/Providers.tsx
+++ src/components/Providers.tsx
@@ -1,13 +1,16 @@
 'use client';
 
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { SessionProvider } from 'next-auth/react';
 import { PropsWithChildren } from 'react';
 
 const queryClient = new QueryClient();
 
 const Providers = ({ children }: PropsWithChildren) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <SessionProvider>{children}</SessionProvider>
+    </QueryClientProvider>
   );
 };
 
